4ab654efffc2bf3ad6dcc252550fb404f51c8d3c
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-06-17 15:03:58 +0800

    chore(config files): configured react testing library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    'airbnb'
+    'airbnb',
+    'plugin:jest/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'jest'],
   rules: {
     'react-refresh/only-export-components': 'warn',
     'comma-dangle': ['error', { functions: 'ignore' }]
