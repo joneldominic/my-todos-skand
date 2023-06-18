b2c2735063d8aed0d53e072dc9f858944fc5b45c
Jonel Dominic Tapang <joneldominictapang.dev@gmail.com>
2023-06-18 22:55:34 +0800

    ci(pulumi): set-up working pulumi
    Can deploy to s3 bucket with dev or prod stack

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,39 +1,89 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable prefer-const */
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
-import * as awsx from '@pulumi/awsx';
+import * as mime from 'mime';
+import * as fs from 'fs';
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket('my-todos-skand-bucket', {
-  website: {
-    indexDocument: 'index.html'
+const APP_NAME = 'my-todos-skand';
+
+function uploadToS3(
+  buildDir: string,
+  bucket: aws.s3.Bucket,
+  publicAccessBlock: aws.s3.BucketPublicAccessBlock,
+  subDir = ''
+) {
+  for (let item of require('fs').readdirSync(`${buildDir}${subDir}`)) {
+    const filePath = require('path').join(buildDir, subDir, item);
+    if (fs.statSync(filePath).isDirectory()) {
+      uploadToS3(`${buildDir}`, bucket, publicAccessBlock, `${subDir}/${item}`);
+    } else {
+      const object = new aws.s3.BucketObject(
+        subDir.length > 0 ? `${subDir.slice(1)}/${item}` : item,
+        {
+          bucket,
+          source: new pulumi.asset.FileAsset(filePath),
+          contentType: mime.getType(filePath) || undefined,
+          acl: 'public-read'
+        },
+        { dependsOn: publicAccessBlock }
+      );
+    }
   }
-});
+}
 
-const ownershipControls = new aws.s3.BucketOwnershipControls('ownership-controls', {
-  bucket: bucket.id,
-  rule: {
-    objectOwnership: 'ObjectWriter'
-  }
-});
+function getAppConfig(app: string) {
+  const stackConfig = new pulumi.Config(app);
+  return {
+    stack: pulumi.getStack(),
+    targetDomain: stackConfig.require('targetDomain')
+  };
+}
 
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock('public-access-block', {
-  bucket: bucket.id,
-  blockPublicAcls: false
-});
-
-const bucketObject = new aws.s3.BucketObject(
-  'index.html',
-  {
+function configureBucketAccessControls(bucket: aws.s3.Bucket) {
+  const ownershipControls = new aws.s3.BucketOwnershipControls(`${bucket.id}-ownership-controls`, {
     bucket: bucket.id,
-    source: new pulumi.asset.FileAsset('./index.html'),
-    contentType: 'text/html',
-    acl: 'public-read'
-  },
-  { dependsOn: publicAccessBlock }
-);
+    rule: {
+      objectOwnership: 'ObjectWriter'
+    }
+  });
 
-export const bucketName = bucket.id;
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+  const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(`${bucket.id}-public-access-block`, {
+    bucket: bucket.id,
+    blockPublicAcls: false
+  });
+
+  return {
+    ownershipControls,
+    publicAccessBlock
+  };
+}
+
+function main() {
+  const { stack, targetDomain } = getAppConfig(APP_NAME);
+
+  const bucket = new aws.s3.Bucket(APP_NAME, {
+    bucket: targetDomain,
+    website: {
+      indexDocument: 'index.html'
+    }
+  });
+
+  const buildDirectory = `${process.cwd()}\\..\\dist`;
+  const { publicAccessBlock } = configureBucketAccessControls(bucket);
+
+  uploadToS3(buildDirectory, bucket, publicAccessBlock);
+
+  return {
+    [stack]: {
+      bucketName: bucket.id,
+      websiteUrl: pulumi.interpolate`http://${bucket.websiteEndpoint}`
+    }
+  };
+}
+
+export default main();
